a41f608841a3b9cbf0ca6808568cd63a10e72b57
Badhon sarker <badhonsarker700@gmail.com>
2023-05-02 00:25:50 +0600

    error page Design

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h2>
-                not fouond
+        <div className="min-h-screen flex flex-col items-center justify-center bg-red-500 text-white py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold">
+              Oops! Something went wrong.
             </h2>
+          </div>
+          <div className="mt-8">
+            <div className="text-center">
+              <p className="text-base font-medium">
+                Please try again later or contact support if the problem persists.
+              </p>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
