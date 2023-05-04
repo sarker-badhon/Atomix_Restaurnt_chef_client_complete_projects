19b88034213268b7750a4f3fbc5da8d6701eb15e
Badhon sarker <badhonsarker700@gmail.com>
2023-05-04 09:34:29 +0600

    contact component page added

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Contacts = () => {
     return (
         <div>
-            <h2>This is Contacts page </h2>
+            <h2 className="text center font-bold text-3xl mt-20">This is Contacts page </h2>
         </div>
     );
 };
