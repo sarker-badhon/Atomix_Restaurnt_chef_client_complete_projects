045b5a8f55d94fb511b8a675237a9caa75e02fda
Badhon sarker <badhonsarker700@gmail.com>
2023-05-04 09:25:11 +0600

    blog page added

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-          <h2 className='text-center mt-10'>This is Blogs page</h2>  
+          <h2 className='text-center mt-10'>This is Blogs page........</h2>  
         </div>
     );
 };
